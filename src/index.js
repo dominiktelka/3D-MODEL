13af2d37a3408e8b250e3bb9ab9938376dbc02f7
Dominik Telka <dominiktelka@gmail.com>
2023-07-12 17:56:52 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM, {createRoot} from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { HashRouter } from "react-router-dom";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App/>);
+//
+// const domNode = document.getElementById('root');
+// const root = createRoot(domNode);
+// root.render(<App />);
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<App />);
+import { HashRouter } from "react-router-dom";
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <HashRouter>
+       <App/>
+    </HashRouter>
+);

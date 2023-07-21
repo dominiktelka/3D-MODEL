5c62d645bbcc02f8e709284a2d05d57c4c85c2b7
dominiktelka <dominiktelka@gmail.com>
2023-07-21 16:57:55 +0200

    added game logic

--- src/sections/DinoSection.js
+++ src/sections/DinoSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styled from "styled-components";
 import "../index.css"
 
@@ -8,7 +8,7 @@ const Section = styled.div`
 
 
 const Title = styled.div`
-    
+
 `
 
 
@@ -17,30 +17,64 @@ export default function DinoGame() {
     const dino = document.getElementById("dino");
     const cactus = document.getElementById("cactus");
     const [isActive, setIsActive] = useState(false);
+    const dinoRef = useRef(null)
+    const cactusRef = useRef(null);
+    const [score, setScore] = useState(0);
+
 
     useEffect(()=>{
         window.addEventListener("keydown",handleKeyDown);
         window.addEventListener("touchstart",handleKeyDown)
+
+        const isAlive = setInterval(checkCollision, 10);
+
         return () =>{
             window.removeEventListener("keydown",handleKeyDown)
             window.addEventListener("touchstart",handleKeyDown)
+            clearInterval(isAlive);
         }
-    },[])
+    },[]);
+
 
 
     const handleKeyDown = event => {
-        // 👇️ toggle isActive state on click
-
         setIsActive(true);
-        setTimeout(()=>{setIsActive(false)},300)
+        cactusRef.current.style.animation = `block 1s infinite linear`
+        setScore((prevScore) => prevScore +1)
+            setTimeout(()=>{setIsActive(false)},300)
     };
 
+    const checkCollision = () => {
+        if (dinoRef.current && cactusRef.current) {
+
+            //here we are taking position of dino and cactus
+            const dinoTop = parseInt(
+                window.getComputedStyle(dinoRef.current).getPropertyValue("top")
+            );
+            const cactusLeft = parseInt(
+                window.getComputedStyle(cactusRef.current).getPropertyValue("left")
+            );
+            
+
+            if (cactusLeft < 45 && cactusLeft > 0 && dinoTop >= 140) {
+                alert("Game over")
+
+                if (cactusRef.current && dinoRef.current) {
+                    cactusRef.current.style.animation = 'none';
+                    setScore(0)
+                }
+            } else{
+                // setScore((prevScore) => prevScore +0.1)
+            }
+        }
+    };
 
 
     return (
         <div className="game">
-            <div className={`dino ${isActive ?"jump" : ''}`}></div>
-            <div className="cactus"></div>
+            <div className="score">Score: {score}</div>
+            <div ref={dinoRef} className={`dino ${isActive ?"jump" : ''}`}></div>
+            <div ref={cactusRef} className="cactus"></div>
         </div>
     )
 }
